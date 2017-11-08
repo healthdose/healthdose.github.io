699adc9f4eb2dce5901eb58ef219f9ead939a6e8
Henry Zhou <henry@henrys-mbp.home>
2017-11-07 20:20:40 -0800

    updated web app

--- js/main.js
+++ js/main.js
@@ -1,28 +1,18 @@
-//side menu toggle
+//Sidebar menu toggle
 $(document).ready(function() {
     $('.sidebar-btn').click(function() {
         $('.sidebar').toggleClass('visible');
     });
 });
 
-
-//filter dropdown created with help from https://www.w3schools.com/howto/howto_js_dropdown.asp
-//toggles button on user click
-function myFunction() {
-    document.getElementById("show-filter").classList.toggle("show");
-}
-
-//close filter when user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.filterbtn')) {
-
-    var dropdowns = document.getElementsByClassName("filter-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+//page change
+$(function(){
+    // bind change event to select
+    $('#select-custom-1').bind('change', function () {
+        var url = $(this).val(); // get selected value
+        if (url) { // require a URL
+            $.mobile.changePage( url, { transition: "slide", changeHash: false });
+        }
+        return false;
+    });
+});
