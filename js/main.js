12e9bc659e23269bd46c64c3581f1d2546002616
SunyueLin <sul104@ucsd.edu>
2017-11-07 23:28:09 -0800

    create profile form

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ $(document).ready(function() {
     $('.sidebar-btn').click(function() {
         $('.sidebar').toggleClass('visible');
     });
+    
+    // New
+    //Profile form subit
+    $('#form-submit-button').click(function(e) {
+        e.preventDefault();
+        var entered_name = $('#name-textField').val();
+        var age = $('#age-textField').val();
+        var gender = $('#gender-dropdown').val();
+        var height = $('#height-textField').val();
+        var weight = $('#weight-textField').val();
+        var allergy = $('#allergy-textField').val();
+        var concern = $('#health-concern-textField').val();
+          
+        $('#profile-name').text(entered_name);
+        $('#profile-gender-age').text(gender + " | " + age);
+        $('#welcome-text').text("Weclome, " + entered_name);
+      
+        $('#detail-name').text(entered_name);
+        $('#detail-height').text(height);
+        $('#detail-weight').text(weight);
+        $('#detail-allergy').text(allergy);
+        $('#detail-health-concern').text(concern);
+    });
 });
 
 //page change
