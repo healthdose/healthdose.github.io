7280577c168d1fc53ca237e41542750f08baaf19
sulhez <33274093+sulhez@users.noreply.github.com>
2017-12-02 19:43:07 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,16 +23,29 @@ $(document).ready(function() {
         var weight = $('#weight-textField').val();
         var allergy = $('#allergy-textField').val();
         var concern = $('#health-concern-textField').val();
+        var landing_create_btn = $('#original-create-btn').val();
+        var profile_create_btn = $('#profile-edit-btn').val();
           
         $('#profile-name').text(entered_name);
-        $('#profile-gender-age').text(gender + " | " + age);
-        $('#welcome-text').text("Welcome, " + entered_name);
+        $('.profile-gender-age').text(gender + " | " + age);
+        // $('#welcome-text').text("Welcome, " + entered_name);
+        $('#landing-user-name').text(entered_name);
       
         $('#detail-name').text(entered_name);
         $('#detail-height').text(height);
         $('#detail-weight').text(weight);
         $('#detail-allergy').text(allergy);
         $('#detail-health-concern').text(concern);
+
+        // Change Create Profile to Edit Profile once their profile has been set
+        $('#original-create-btn').text('Edit Profile');
+        $('#profile-edit-btn').text('Edit Profile');
+
+        $('.edit-profile-container').fadeOut();
+    });
+
+    $('#profile-edit-btn').click(function() {
+        $('.edit-profile-container').fadeIn();
     });
 
     // Filter recipe cards
@@ -53,6 +66,36 @@ $(document).ready(function() {
         window.location = $(this).find('.view-recipe').attr('href');
         return false;
     });
+
+
+    /*// Click Handler; Registers to google analytics once the button is clicked
+    $("#original-create-btn").click(function() { // click handler for original design; create button
+        console.log("Create Profile button clicked original"); // fix smart quote from slide!
+        // tracker code here, refer to slide #26 and #27
+        tracker = ga.getAll()[0];
+        tracker.send('event', 'Create Profile', 'click');
+    });
+
+    $(".view-recipes-og").click(function() { // click handler for original design; Go to recipes button
+        console.log("Go to Recipes button clicked original");
+        // tracker code here, refer to slide #26 and #27
+        tracker = ga.getAll()[0];
+        tracker.send('event', 'Go to Recipes', 'click');
+    });
+
+    $(".create-profile-btn").click(function() { // click handler for redesign; create button
+        console.log("Create Profile button clicked redesign");
+        // tracker code here, refer to slide #26 and #27
+        tracker = ga.getAll()[0];
+        tracker.send('event', 'Create Profile', 'click');
+    });
+
+    $(".view-recipes-redesign").click(function() { // click handler for redesign; Go to recipes button
+        console.log("Go to Recipes button clicked redesign");
+        // tracker code here, refer to slide #26 and #27
+        tracker = ga.getAll()[0];
+        tracker.send('event', 'Go to Recipes', 'click');
+    });*/
 });
 
 //page change
